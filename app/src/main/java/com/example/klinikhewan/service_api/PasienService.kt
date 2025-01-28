4514c350768cb3037d13cb76fc3b260b372fadf0
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-01-28 16:50:56 +0700

    menambahkan dan membuat pasien service

---
+++ app/src/main/java/com/example/klinikhewan/service_api/PasienService.kt
@@ -0,0 +1,36 @@
+package com.example.klinikhewan.service_api
+
+import com.example.klinikhewan.model.AllPasienResponse
+import com.example.klinikhewan.model.Pasien
+import com.example.klinikhewan.model.PasienDetailResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface PasienService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @POST("pasien/store")
+    suspend fun insertPasien(@Body pasien: Pasien)
+
+    @GET("pasien/.")
+    suspend fun getAllPasien(): AllPasienResponse
+
+    @GET("pasien/{id_hewan}")
+    suspend fun getPasienById(@Path("id_hewan") idhewan: String): PasienDetailResponse
+
+    @PUT("pasien/{id_hewan}")
+    suspend fun updatePasien(@Path("id_hewan") idhewan: String, @Body pasien: Pasien)
+
+    @DELETE("pasien/{id_hewan}")
+    suspend fun deletePasien(@Path("id_hewan") idhewan: String): Response<Void>
+
+}

b0a8ff12e04001f7e8859cd6259bb86f7ded4150
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2025-01-28 17:55:55 +0700

    menambahkan home,insert,detail dan update viewmodel untuk jenis

---
+++ app/src/main/java/com/example/klinikhewan/ui/pages/jenishewan/viewmodel/InsertJnsViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.klinikhewan.ui.pages.jenishewan.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.klinikhewan.model.Jenis
+import com.example.klinikhewan.repository.JenisRepository
+
+import kotlinx.coroutines.launch
+
+class InsertJnsViewModel( private val jns: JenisRepository): ViewModel(){
+    var uiStateJns by mutableStateOf(InsertJnsUiState())
+        private set
+
+    fun updateInsertJnsState(insertJnsUiEvent: InsertJnsUiEvent){
+        uiStateJns = InsertJnsUiState(
+            insertJnsUiEvent = insertJnsUiEvent
+        )
+    }
+
+    suspend fun insertJns(){
+        viewModelScope.launch {
+            try {
+                jns.insertJenis(uiStateJns.insertJnsUiEvent.toJenis())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+
+
+
+data class InsertJnsUiState(
+    val insertJnsUiEvent: InsertJnsUiEvent = InsertJnsUiEvent(),
+)
+
+data class InsertJnsUiEvent(
+    val idjenishewan: String = "",
+    val namajenishewan: String = "",
+    val deskripsi: String = ""
+)
+
+fun InsertJnsUiEvent.toJenis(): Jenis = Jenis(
+    idjenishewan = idjenishewan,
+    namajenishewan = namajenishewan,
+    deskripsi = deskripsi
+)
+
+//UntukJnsEvent
+fun Jenis.toInsertJenisUiEvent(): InsertJnsUiEvent = InsertJnsUiEvent(
+    idjenishewan = idjenishewan,
+    namajenishewan = namajenishewan,
+    deskripsi = deskripsi
+)
+
+fun Jenis.toUiStateJns(): InsertJnsUiState = InsertJnsUiState(
+    insertJnsUiEvent = toInsertJenisUiEvent()
+)
---
+++ app/src/main/java/com/example/klinikhewan/ui/pages/jenishewan/viewmodel/UpdateJenisViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.klinikhewan.ui.pages.jenishewan.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.klinikhewan.repository.JenisRepository
+import com.example.klinikhewan.ui.navigation.UpdateJenis
+
+import kotlinx.coroutines.launch
+
+class UpdateJenisViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val jns: JenisRepository
+): ViewModel(){
+    var updateJnsUiState by mutableStateOf(InsertJnsUiState())
+        private set
+
+    private val _idJenishewan: String = checkNotNull(savedStateHandle[UpdateJenis.idJENIS])
+
+    init {
+        viewModelScope.launch {
+            updateJnsUiState = jns.getJenisById(_idJenishewan)
+                .toUiStateJns()
+        }
+    }
+
+    fun updateInsertJnsState(insertJnsUiEvent: InsertJnsUiEvent){
+        updateJnsUiState = InsertJnsUiState(
+            insertJnsUiEvent = insertJnsUiEvent
+        )
+    }
+
+    suspend fun updateJenis(){
+        viewModelScope.launch {
+            try {
+                jns.updateJenis(_idJenishewan, updateJnsUiState.insertJnsUiEvent.toJenis())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
